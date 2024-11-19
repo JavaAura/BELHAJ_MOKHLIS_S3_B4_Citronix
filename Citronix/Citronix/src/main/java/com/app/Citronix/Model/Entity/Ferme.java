a02046869554aae895b837e55138cfff6f3aa3e4
BelhajMokhlis <belhajmokhlis@gmail.com>
2024-11-19 20:25:48 +0100

    feat:Entity Ferme

---
+++ Citronix/Citronix/src/main/java/com/app/Citronix/Model/Entity/Ferme.java
@@ -0,0 +1,42 @@
+package com.app.Citronix.Model.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "fermes")
+public class Ferme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @NotNull(message = "Le nom de la ferme est obligatoire")
+    @Size(min = 2, max = 100, message = "Le nom doit contenir entre 2 et 100 caractères")
+    private String nom;
+
+    @Column(nullable = false)
+    @NotNull(message = "La localisation est obligatoire")
+    @Size(min = 2, max = 200, message = "La localisation doit contenir entre 2 et 200 caractères")
+    private String localisation;
+
+    @Column(nullable = false)
+    @NotNull(message = "La superficie est obligatoire")
+    @Positive(message = "La superficie doit être supérieure à 0")
+    @DecimalMin(value = "0.1", message = "La superficie minimale d'un champ doit être de 0.1 hectare")
+    private Double superficie;
+
+    @Column(name = "date_creation")
+    @PastOrPresent(message = "La date de création ne peut pas être dans le futur")
+    private LocalDate dateCreation;
+} 
