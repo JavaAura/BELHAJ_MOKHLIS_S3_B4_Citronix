e5edb7cb2aded679cdac393857285baa0725d6c9
BelhajMokhlis <belhajmokhlis@gmail.com>
2024-11-21 17:16:18 +0100

    feat: ChampRepository

---
+++ Citronix/Citronix/src/main/java/com/app/Citronix/Repository/ChampRepository.java
@@ -0,0 +1,10 @@
+package com.app.Citronix.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.Citronix.Model.Entity.Champ;
+
+@Repository
+public interface ChampRepository extends JpaRepository<Champ, Integer> {
+} 
